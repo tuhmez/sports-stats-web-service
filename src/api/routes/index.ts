5dd5ca6ed94916bfa912a9b86e74c838f485d521
Chris Tamez <ctamez232@gmail.com>
2022-08-12 09:40:42 -0500

    updating version

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -4,11 +4,13 @@ import { PingRoutes } from './ping';
 
 const router = express.Router();
 
+const version = process.env.npm_package_version || process.env.VERSION;
+
 router.get('/', async (_req, res) => {
   const baseURL = `${_req.protocol}://${_req.get('host')}${_req.originalUrl}`;
   const html = `
     <div style="display: flex; flex-direction: column; align-items: flex-start; gap: 4px;">
-      <h1>Sports Stats Web Service - v${process.env.npm_package_version}</h1>
+      <h1>Sports Stats Web Service - v${version}</h1>
       <p>Listed are the base routes for the web service. When clicking the link, you will be redirected to the base of the selected route, where more routes will be listed or data will be returned.</p>
       <div style="display: flex; flex-direction: column; align-items: flex-start; gap: 8px;">
         <a href="${baseURL}docs">Swagger UI</a>
