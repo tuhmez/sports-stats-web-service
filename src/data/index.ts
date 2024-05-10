abcd58f4ca85a10a22cdc8bde7c350d150e36274
Chris Tamez <ctamez232@gmail.com>
2024-05-10 11:07:53 -0500

    added match up graphics

---
+++ src/data/index.ts
@@ -0,0 +1,18 @@
+interface ILogo {
+  useSecondaryColor: string[];
+  useTeamCapOnDark: string[];
+  urlTeamCapOnDark: string;
+  useOtherUrl: string[];
+  otherUrls: any;
+}
+
+export const logos: ILogo = {
+  useSecondaryColor: ['bal', 'tex', 'oak', 'sf'],
+  useTeamCapOnDark: ['nyy', 'tb', 'det', 'stl', 'wsh', 'col', 'lad', 'sd', 'cin'],
+  urlTeamCapOnDark: 'https://www.mlbstatic.com/team-logos/team-cap-on-dark',
+  useOtherUrl: ['min', 'tex'],
+  otherUrls: {
+    min: 'https://images.ctfassets.net/iiozhi00a8lc/t142_header_primaryMN_TC_19_blue_svg/c0f95eb9469a7d98b24498395d4f4dc9/MIN23_Dark_Active.svg',
+    tex: 'https://images.ctfassets.net/iiozhi00a8lc/6HSwMP9twv0hrrHVF4Nyo0/0f0063a3fb5f029ac4dadd420b1f1142/140.svg'
+  }
+};
